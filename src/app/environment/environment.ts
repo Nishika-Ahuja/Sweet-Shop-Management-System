d37cee44a3032eb53cb6fcd4df33f2892d56af35
Nishika-Ahuja <nishikaahuja209@gmail.com>
2025-12-15 01:45:03 +0530

    fix: add Netlify redirect for Angular routing

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://127.0.0.1:8000/api'
+  apiUrl: 'https://sweetshop-backend-f0ru.onrender.com/api'
 };
--- sweet-backend/main.py
+++ sweet-backend/main.py
@@ -12,7 +12,7 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI(title="Sweet Shop Management System")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:4200"],  
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"], 
     allow_headers=["*"],  
